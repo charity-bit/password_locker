0213d3b11cced04cf0f6716b0d71e39eef5dac6d
charity-bit <charitynyanchera@gmail.com>
2022-04-23 19:38:36 +0300

    unit test for User init method

--- plocker_test.py
+++ plocker_test.py
@@ -1,4 +1,16 @@
 import unittest
 from plocker import User
 
-class TestUser(unittest.TestCase)
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User("charity",'qwertyip')
+        
+    def test_init(self):
+        self.assertEqual(self.new_user.username,"charity")
+        self.assertEqual(self.new_user.password,"qwertyip")
+        
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
