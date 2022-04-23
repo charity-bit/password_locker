b91350749b9341767d284b763983bef60e28170a
charity-bit <charitynyanchera@gmail.com>
2022-04-23 19:42:42 +0300

    created User class and initilized the properties

--- plocker_test.py
+++ plocker_test.py
@@ -2,9 +2,19 @@ import unittest
 from plocker import User
 
 class TestUser(unittest.TestCase):
+    
+    '''
+    set up method to run before each test
+    create a new user before any test
+    '''
     def setUp(self):
         self.new_user = User("charity",'qwertyip')
         
+        
+    '''
+    test_init to test if the object is initialized properly
+    
+    '''
     def test_init(self):
         self.assertEqual(self.new_user.username,"charity")
         self.assertEqual(self.new_user.password,"qwertyip")
