67c30dfc38e97131f89418cbc128eac03abdbb90
charity-bit <charitynyanchera@gmail.com>
2022-04-23 20:17:23 +0300

    ft:test test_remove user from the system

--- plocker_test.py
+++ plocker_test.py
@@ -36,9 +36,11 @@ class TestUser(unittest.TestCase):
     def test_remove_user(self):
         self.new_user.save_user()
         test_user = User("nyanchera","12345")
-        
-        
-        
+        test_user.save_user()
+        self.new_user.remove_user()
+        self.assertEqual(len(User.users),1)
+    
+    
         
         
         
