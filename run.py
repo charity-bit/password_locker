9d437cd296a6f4a5a1e3efc76fe51b1393282fa3
charity-bit <charitynyanchera@gmail.com>
2022-04-24 09:50:27 +0300

    created update credentials,create credential functions

--- run.py
+++ run.py
@@ -12,3 +12,14 @@ def s_user(user):
 def r_user(user):
     user.remove_user()
     
+def create_creds(site_name,username,password):
+    new_creds = Credentials(site_name,username,password)
+    return new_creds
+
+def s_creds(creds):
+    creds.save_creds()
+    
+def u_cred(creds):
+    return creds.update_cred()
+
+    
